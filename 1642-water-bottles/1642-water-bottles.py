f3273e9a3e151af9c1398524ef3569789d8c1893
Fazeel Ahmed Khan <83122423+FazeelAhmedKhan-dev@users.noreply.github.com>
2025-10-01 10:30:09 +0500

    Time: 0 ms (100%), Space: 17.6 MB (98.26%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        
+        while numBottles >= numExchange:
+            numBottles -= numExchange
+            numBottles += 1
+            ans += 1
+        return ans
