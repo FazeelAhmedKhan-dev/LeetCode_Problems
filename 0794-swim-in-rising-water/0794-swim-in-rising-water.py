03678161148759dd11b43d96da8b8136bc21450b
Fazeel Ahmed Khan <83122423+FazeelAhmedKhan-dev@users.noreply.github.com>
2025-10-07 08:23:49 +0500

    Time: 27 ms (66.79%), Space: 18.4 MB (72.3%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -2,21 +2,19 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
         visited = set()
-        min_heap = [(grid[0][0], 0, 0)]
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        res = 0
+        minH = [[grid[0][0], 0, 0]]
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-        while min_heap:
-            t, i, j = heapq.heappop(min_heap)
-            res = max(res, t)
-            if (i, j) == (n - 1, n - 1):
-                return res
-            if (i, j) in visited:
-                continue
-            visited.add((i, j))
+        visited.add((0,0))
 
-            for di, dj in directions:
-                ni, nj = i + di, j + dj
-                if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
-                    heapq.heappush(min_heap, (grid[ni][nj], ni, nj))
-        return t
+        while minH:
+            t, r, c = heapq.heappop(minH)
+            if r == n-1 and c == n-1:
+                return t
+            
+            for dr, dc in directions:
+                neiR, neiC = dr + r, dc + c
+                if neiR < 0 or neiC < 0 or neiR == n or neiC == n or (neiR, neiC) in visited:
+                    continue
+                visited.add((neiR, neiC))
+                heapq.heappush(minH, [max(t, grid[neiR][neiC]),neiR, neiC])
