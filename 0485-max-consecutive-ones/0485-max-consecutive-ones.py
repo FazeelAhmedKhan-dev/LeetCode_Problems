43541162fcff543b2911ff59b635073ce95f7654
Fazeel Ahmed Khan <83122423+FazeelAhmedKhan-dev@users.noreply.github.com>
2025-12-05 15:34:00 +0500

    Time: 5912 ms (5.19%), Space: 21.6 MB (5.45%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,11 +3,13 @@ class Solution:
         left = 0
         curr_sum = 0
         max_sum = 0
+
         for right in range(len(nums)):
             if nums[right] == 0:
-                left = right+1
+                left = right + 1
                 continue
+            
             curr_sum = len(nums[left:right+1])
             max_sum = max(curr_sum, max_sum)
-
+        
         return max_sum
