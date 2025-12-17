930e8e28d68a2cb96865f8a486fac17c3fd8e11a
Fazeel Ahmed Khan <83122423+FazeelAhmedKhan-dev@users.noreply.github.com>
2025-12-17 15:53:24 +0500

    Time: 0 ms (100%), Space: 17.7 MB (98.91%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,7 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
 
+        res = []
         subset = []
 
         def dfs(i):
@@ -14,7 +14,6 @@ class Solution:
 
             subset.pop()
             dfs(i+1)
-        
+
         dfs(0)
         return res
-        
