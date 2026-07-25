d2a13d9de3ebdcc43146c3d314e94e35028dad98
Fazeel Ahmed Khan <83122423+FazeelAhmedKhan-dev@users.noreply.github.com>
2026-07-25 19:35:34 +0500

    Time: 14 ms (9.75%), Space: 24 MB (5.3%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        left = 0
-        right = len(s) - 1
+        palindrome = []
 
-        while left < right:
+        for val in s:
+            ascii_val = ord(val)
 
-            while left < right and not self.alphaNum(s[left]):
-                left += 1
-            
-            while right > left and not self.alphaNum(s[right]):
-                right -= 1
+            if ((ascii_val >= 48 and ascii_val <= 57)
+                or (ascii_val >= 65 and ascii_val <= 90)
+                or (ascii_val >= 97 and ascii_val <= 122)):
+                
+                palindrome.append(val.lower())
 
-            if s[left].lower() != s[right].lower():
-                return False
-            
-            left += 1
-            right -= 1
         
-        return True
-    
-    def alphaNum(self, c):
-        return (ord("A") <= ord(c) <= ord("Z") or
-                ord("a") <= ord(c) <= ord("z") or
-                ord("0") <= ord(c) <= ord("9"))
+        return palindrome == palindrome[::-1]
+        
